7819dc29503d6fe5ff4d04e672224a16ecf8c4a8
Generil de los Santos <generild@gmail.com>
2017-12-03 22:57:30 +0800

    Add graphs

--- part1.py
+++ part1.py
@@ -1,5 +1,4 @@
 import math
-import matplotlib.pyplot as plot
 
 
 def initialize():
@@ -98,56 +97,17 @@ def main():
 
     prev_j = 0
     tmp_c = clusters
-    c1 = [clusters[0]]
-    c2 = [clusters[1]]
-    c3 = [clusters[2]]
     while iterate < 10:
         m1, m2, m3, jc = assign_cluster(tmp_c, dataset)
         tmp_c = move_centroid(m1, m2, m3)
-        c1.append(tmp_c[0])
-        c2.append(tmp_c[1])
-        c3.append(tmp_c[2])
         cm_file = open("iter{}_cm.txt".format(iterate + 1), 'a')
         cost_j = compute_j(jc)
-        diff_j = cost_j - prev_j
+        diff_j = abs(prev_j - cost_j)
         cm_file.write('J = {:0.6f}\n'.format(cost_j))
         cm_file.write('dJ = {:0.6f}\n'.format(diff_j))
         cm_file.close()
         prev_j = cost_j
         iterate += 1
-
-    x, y = [], []
-    for item in dataset:
-        x.append(item[0])
-        y.append(item[1])
-
-    plot.tight_layout()
-    plot.plot(x, y, 'k.')
-
-    x, y = [], []
-    for item in c1:
-        x.append(item[0])
-        y.append(item[1])
-
-    plot.plot(x, y, 'ro')
-
-    x, y = [], []
-    for item in c2:
-        x.append(item[0])
-        y.append(item[1])
-
-    plot.plot(x, y, 'mo')
-
-    x, y = [], []
-    for item in c3:
-        x.append(item[0])
-        y.append(item[1])
-
-    plot.plot(x, y, 'bo')
-
-    plot.axis([0, 10, 0, 10])
-    plot.show()
-
 
 if __name__ == "__main__":
     main()
